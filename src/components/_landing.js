81e0c6d9bba7c12b6d01f2d7a61bbafb95baf0b1
picpicpicpicnic <sscanlan@deeplocal.com>
2025-02-24 15:14:44 -0500

    Fonts, icons

--- src/components/_landing.js
+++ src/components/_landing.js
@@ -1,8 +1,25 @@
+import { useRef, useState, useEffect } from 'react';
 import Flower from './icons/_flower';
+import FlowerWink from './icons/_flower-wink';
 const Landing = () => {
+  const [winking, setWinking] = useState(false);
+  const winkingRef = useRef(false);
+  useEffect(() => {
+    console.log('does this even run');
+    const wink = setInterval(() => {
+      console.log('we should change this', winkingRef.current);
+      winkingRef.current = !winkingRef.current;
+      setWinking(winkingRef.current);
+    }, 2000);
+    return () => clearInterval(wink);
+  }, []);
   return (
     <section className="min-h-[100vh] flex flex-col items-center justify-center">
-      <Flower className="w-[100%] h-[100%] max-h-[100vh] max-w-[100%]" />
+      {winking ? (
+        <FlowerWink className="w-[100%] h-[100%] max-h-[100vh] max-w-[100%]" />
+      ) : (
+        <Flower className="w-[100%] h-[100%] max-h-[100vh] max-w-[100%]" />
+      )}
     </section>
   );
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap"
           rel="stylesheet"
-        />
+          href="https://use.typekit.net/qtc3dpj.css"
+        ></link>
       </Head>
       <body className="antialiased">
         <Main />
